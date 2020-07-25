537e05dc6461995e1a971ede4518b865bc178cfa
S. Irfan Ali <syedirfanali@outlook.com>
2020-07-25 15:59:07 +0530

    Add project files.

---
+++ TestPractice/Pages/ScrollAnimate/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TestPractice.Pages.ScrollAnimate
+{
+    public class ScrollAnimateModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
